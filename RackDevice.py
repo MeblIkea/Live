052f84095232c8457e7b71c8075865a654088d19
MeblIkea <69876322+MeblIkea@users.noreply.github.com>
2024-08-27 02:55:47 +0200

    Worked on the Rack and did un necessary programs

--- RackDevice.py
+++ RackDevice.py
@@ -1,4 +1,4 @@
-﻿# Have to finish Rack Device
+﻿# All referenced (missing descriptions)
 from typing import Callable
 from Live.Device import Device
 
@@ -110,3 +110,282 @@ class RackDevice(Device):
             """
             return False
 
+    @property
+    def can_show_chains(self) -> bool:
+        """
+        :return: True if this Rack contains a rack instrument device
+        that is capable of showing its chains in session view.
+        """
+        return False
+
+    @property
+    def chains(self):
+        """
+        :return: Return const access to the list of chains in this device.
+        """
+        return
+
+    @property
+    def drum_pads(self):
+        """
+        :return: Return const access to the list of drum pads in this device.
+        """
+        return
+
+    @property
+    def has_drum_pads(self) -> bool:
+        """
+        :return: Returns true if the device is a drum rack which has drum pads.
+        """
+        return False
+
+    @property
+    def has_macro_mappings(self) -> bool:
+        """
+        :return: Returns true if any of the rack's macros are mapped to a parameter.
+        """
+        return False
+
+    @property
+    def is_showing_chains(self) -> bool:
+        """
+        :return: Returns True, if it is showing chains.
+        """
+        return False
+
+    @property
+    def macros_mapped(self):
+        """
+        :return: A list of booleans, one for each macro parameter, which is True iff
+        that macro is mapped to something.
+        """
+        return
+
+    @property
+    def return_chains(self):
+        """
+        :return: Return const access to the list of return chains in this device.
+        """
+        return
+
+    @property
+    def selected_variation_index(self) -> int:
+        """
+        :return: Access to the index of the currently selected macro variation.
+        """
+        return 0
+
+    @property
+    def variation_count(self) -> int:
+        """
+        :return: Access to the number of macro variations currently stored.
+        """
+        return 0
+
+    @property
+    def visible_drum_pads(self):
+        """
+        :return: Return const access to the list of visible drum pads in this device.
+        """
+        return
+
+    def add_chains_listener(self, arg2: Callable):
+        """
+        Add a listener function or method, which will be called as soon as the property "chains" has changed.
+        """
+        return
+
+    def add_drum_pads_listener(self, arg2: Callable):
+        """
+        Add a listener function or method, which will be called as soon as the property "drum_pads" has changed.
+        """
+        return
+
+    def add_has_drum_pads_listener(self, arg2: Callable):
+        """
+        Add a listener function or method, which will be called as soon as the property "has_drum_pads" has changed.
+        """
+        return
+
+    def add_has_macro_mappings_listener(self, arg2: Callable):
+        """
+        Add a listener function or method, which will be called as soon as the property "has_macro_mappings" has changed.
+        """
+        return
+
+    def add_is_showing_chains_listener(self, arg2: Callable):
+        """
+        Add a listener function or method, which will be called as soon as the property "is_showing_chains" has changed.
+        """
+        return
+
+    def add_macro(self):
+        """
+        Increases the number of visible macro controls in the rack.
+        Throws an exception if the maximum number of macro controls is reached.
+        """
+        return
+
+    def add_macros_mapped_listener(self, arg2: Callable):
+        """
+        Add a listener function or method, which will be called as soon as the property "macros_mapped" has changed.
+        """
+        return
+
+    def add_return_chains_listener(self, arg2: Callable):
+        """
+        Add a listener function or method, which will be called as soon as the property "return_chains" has changed.
+        """
+        return
+
+    def add_visible_drum_pads_listener(self, arg2: Callable):
+        """
+        Add a listener function or method, which will be called as soon as the property "visible_drum_pads" has changed.
+        """
+        return
+
+    def chains_has_listener(self, arg2: Callable) -> bool:
+        """
+        :return: Returns true, if the given listener function or method is connected to the property "chains".
+        """
+        return False
+
+    def copy_pad(self, source_index: int, destination_index: int):
+        """
+        Copies all contents of a drum pad from a source pad into a destination pad.
+        Copy_pad(source_index, destination_index)
+        where source_index and destination_index correspond to the note number/index of the drum pad in a drum rack.
+        Throws an exception when the source pad is empty,
+        or when the source or destination indices are not between 0-127.
+        """
+        return
+
+    def delete_selected_variation(self):
+        """
+        Deletes the currently selected macro variation.
+        Does nothing if there is no selected variation.
+        """
+        return
+
+    def drum_pads_has_listener(self, arg2: Callable) -> bool:
+        """
+        :return: Returns true, if the given listener function or method is connected to the property "drum_pads".
+        """
+        return False
+
+    def has_drum_pads_has_listener(self, arg2: Callable) -> bool:
+        """
+        :return: Returns true, if the given listener function or method is connected to the property "has_drum_pads".
+        """
+        return False
+
+    def has_macro_mappings_has_listener(self, arg2: Callable) -> bool:
+        """
+        :return: Returns true, if the given listener function or method is connected to the property "has_macro_mappings".
+        """
+        return False
+
+    def is_showing_chains_has_listener(self, arg2: Callable) -> bool:
+        """
+        :return: Returns true, if the given listener function or method is connected to the property "is_showing_chains".
+        """
+        return False
+
+    def macros_mapped_has_listener(self, arg2: Callable) -> bool:
+        """
+        :return: Returns true, if the given listener function or method is connected to the property "macros_mapped".
+        """
+        return False
+
+    def randomize_macros(self):
+        """
+        Randomizes the values for all macro controls not excluded from randomization.
+        """
+        return
+
+    def recall_last_used_variation(self):
+        """
+        Recalls the macro variation recalled most recently.
+        Does nothing if no variation has been recalled yet.
+        """
+        return
+
+    def recall_selected_variation(self):
+        """
+        Recalls the currently selected macro variation.
+        Does nothing if there are no variations.
+        """
+        return
+
+    def remove_chains_listener(self, arg2: Callable):
+        """
+        Remove a previously set listener function or method from property "chains".
+        """
+        return
+
+    def remove_drum_pads_listener(self, arg2: Callable):
+        """
+        Remove a previously set listener function or method from property "drum_pads".
+        """
+        return
+
+    def remove_has_drum_pads_listener(self, arg2: Callable):
+        """
+        Remove a previously set listener function or method from property "has_drum_pads".
+        """
+        return
+
+    def remove_has_macro_mappings_listener(self, arg2: Callable):
+        """
+        Remove a previously set listener function or method from property "has_macro_mappings".
+        """
+        return
+
+    def remove_is_showing_chains_listener(self, arg2: Callable):
+        """
+        Remove a previously set listener function or method from property "is_showing_chains".
+        """
+        return
+
+    def remove_macro(self):
+        """
+        Decreases the number of visible macro controls in the rack.
+        Throws an exception if the minimum number of macro controls is reached.
+        """
+        return
+
+    def remove_macros_mapped_listener(self, arg2: Callable):
+        """
+        Remove a previously set listener function or method from property "macros_mapped".
+        """
+        return
+
+    def remove_return_chains_listener(self, arg2: Callable):
+        """
+        Remove a previously set listener function or method from property "return_chains".
+        """
+        return
+
+    def remove_visible_drum_pads_listener(self, arg2: Callable):
+        """
+        Remove a previously set listener function or method from property "visible_drum_pads".
+        """
+        return
+
+    def return_chains_has_listener(self, arg2: Callable) -> bool:
+        """
+        :return: Returns true, if the given listener function or method is connected to the property "return_chains".
+        """
+        return False
+
+    def store_variation(self):
+        """
+        Stores a new variation of the values of all currently mapped macros
+        """
+        return
+
+    def visible_drum_pads_has_listener(self, arg2: Callable) -> bool:
+        """
+        :return: Returns true, if the given listener function or method is connected to the property "visible_drum_pads".
+        """
+        return False
---
+++ substract_xml.py
@@ -0,0 +1,119 @@
+﻿base = """
+<Property>Live.Device.Device._live_ptr<Description>Property</Description></Property>
+
+<Property>Live.Device.Device.can_have_chains<Description>Property</Description></Property>
+
+<Doc>	Returns true if the device is a rack.</Doc>
+
+<Property>Live.Device.Device.can_have_drum_pads<Description>Property</Description></Property>
+
+<Doc>	Returns true if the device is a drum rack.</Doc>
+
+<Property>Live.Device.Device.canonical_parent<Description>Property</Description></Property>
+
+<Doc>	Get the canonical parent of the Device.</Doc>
+
+<Property>Live.Device.Device.class_display_name<Description>Property</Description></Property>
+
+<Doc>	Return const access to the name of the device's class name as displayed in Live's browser and device chain</Doc>
+
+<Property>Live.Device.Device.class_name<Description>Property</Description></Property>
+
+<Doc>	Return const access to the name of the device's class.</Doc>
+
+<Property>Live.Device.Device.is_active<Description>Property</Description></Property>
+
+<Doc>	Return const access to whether this device is active. This will be false bothwhen the device is off and when it's inside a rack device which is off.</Doc>
+
+<Property>Live.Device.Device.name<Description>Property</Description></Property>
+
+<Doc>	Return access to the name of the device.</Doc>
+
+<Property>Live.Device.Device.parameters<Description>Property</Description></Property>
+
+<Doc>	Const access to the list of available automatable parameters for this device.</Doc>
+
+<Property>Live.Device.Device.type<Description>Property</Description></Property>
+
+<Doc>	Return the type of the device.</Doc>
+
+<Property>Live.Device.Device.view<Description>Property</Description></Property>
+
+<Doc>	Representing the view aspects of a device.</Doc>
+
+<Method>Live.Device.Device.add_is_active_listener()<Description>Method</Description></Method>
+
+<Doc>	add_is_active_listener( (Device)arg1, (object)arg2) -&gt; None : Add a listener function or method, which will be called as soon as the property "is_active" has changed. C++ signature :  void add_is_active_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.add_name_listener()<Description>Method</Description></Method>
+
+<Doc>	add_name_listener( (Device)arg1, (object)arg2) -&gt; None : Add a listener function or method, which will be called as soon as the property "name" has changed. C++ signature :  void add_name_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.add_parameters_listener()<Description>Method</Description></Method>
+
+<Doc>	add_parameters_listener( (Device)arg1, (object)arg2) -&gt; None : Add a listener function or method, which will be called as soon as the property "parameters" has changed. C++ signature :  void add_parameters_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.is_active_has_listener()<Description>Method</Description></Method>
+
+<Doc>	is_active_has_listener( (Device)arg1, (object)arg2) -&gt; bool : Returns true, if the given listener function or method is connected to the property "is_active". C++ signature :  bool is_active_has_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.name_has_listener()<Description>Method</Description></Method>
+
+<Doc>	name_has_listener( (Device)arg1, (object)arg2) -&gt; bool : Returns true, if the given listener function or method is connected to the property "name". C++ signature :  bool name_has_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.parameters_has_listener()<Description>Method</Description></Method>
+
+<Doc>	parameters_has_listener( (Device)arg1, (object)arg2) -&gt; bool : Returns true, if the given listener function or method is connected to the property "parameters". C++ signature :  bool parameters_has_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.remove_is_active_listener()<Description>Method</Description></Method>
+
+<Doc>	remove_is_active_listener( (Device)arg1, (object)arg2) -&gt; None : Remove a previously set listener function or method from property "is_active". C++ signature :  void remove_is_active_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.remove_name_listener()<Description>Method</Description></Method>
+
+<Doc>	remove_name_listener( (Device)arg1, (object)arg2) -&gt; None : Remove a previously set listener function or method from property "name". C++ signature :  void remove_name_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.remove_parameters_listener()<Description>Method</Description></Method>
+
+<Doc>	remove_parameters_listener( (Device)arg1, (object)arg2) -&gt; None : Remove a previously set listener function or method from property "parameters". C++ signature :  void remove_parameters_listener(TPyHandle&lt;ADevice&gt;,boost::python::api::object)</Doc>
+
+<Method>Live.Device.Device.store_chosen_bank()<Description>Method</Description></Method>
+
+<Doc>	store_chosen_bank( (Device)arg1, (int)arg2, (int)arg3) -&gt; None : Set the selected bank in the device for persistency. C++ signature :  void store_chosen_bank(TPyHandle&lt;ADevice&gt;,int,int)</Doc>
+""".split("\n")
+extend = open("extract.xml", "r").read().split("\n")
+
+
+def fetch_xml_elements(elements):
+    properties = {}
+    methods = {}
+    for i, element in enumerate(elements):
+        if element.startswith("<Property>"):
+            if elements[i + 2].startswith("<Doc>"):
+                doc = elements[i + 2].split("<Doc>	")[1].split("</Doc>")[0]
+            else:
+                doc = ""
+            properties[element.split("<Property>")[1].split("<Description>")[0]] = doc
+        elif element.startswith("<Method>"):
+            if elements[i + 2].startswith("<Doc>"):
+                doc = \
+                    elements[i + 2].split("<Doc>	")[1].replace("-&gt;", "->").replace(" C++ signature", "").split("</Doc>")[
+                        0]
+            else:
+                doc = ""
+            methods[element.split("<Method>")[1].split("<Description>")[0]] = doc
+    return properties, methods
+
+
+device = fetch_xml_elements(base)
+extended = fetch_xml_elements(extend)
+
+device_stuff = [props.split(".")[-1] for props in device[0]] + [methods.split(".")[-1] for methods in device[1]]
+
+for prop, desc in extended[0].items():
+    if prop.split(".")[-1] not in device_stuff:
+        print(prop, "|", desc)
+
+for method, desc in extended[1].items():
+    if method.split(".")[-1] not in device_stuff:
+        print(method, "|", desc)
